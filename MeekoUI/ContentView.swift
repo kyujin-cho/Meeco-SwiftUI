b7a5e5388cd943be14984f415c91b3731c83cf22
Kyujin Cho <kyujin.cho@lablup.com>
2019-07-19 21:39:24 +0900

    Initial Commit

---
+++ MeekoUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MeekoUI
+//
+//  Created by Kyujin Cho on 7/19/19.
+//  Copyright © 2019 Kyujin Cho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
